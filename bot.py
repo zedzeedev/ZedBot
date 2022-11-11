bd4d4fd2e37c7101346bb2cc186004d3d016bb2a
Zedzee <65473813+transbee@users.noreply.github.com>
2022-11-11 18:02:09 -0500

    cmds
    csdas

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import json
+from time import sleep
 
 bot = discord.Bot()
 
@@ -25,6 +26,38 @@ cogs_list = [
 
 for cog in cogs_list:
     bot.load_extension(f"cogs.{cog}")
+
+
+@bot.command()
+async def lesbian(ctx):
+    await ctx.respond("yeah, thats me")
+
+
+@bot.command(description="Forces the bot to send the given message.")
+async def talk(ctx, msg: str):
+    await ctx.respond("Message sending...", ephemeral=True)
+    await ctx.send(msg)
+
+
+stop_cmd = False
+@bot.command(description="WARNING: DO NOT USE")
+async def avery(ctx):
+    stop_cmd = False
+    if stop_cmd == False:
+        for i in range(0, 100):
+            if stop_cmd == True:
+                break
+            else:
+                await ctx.respond("omg thats so fucking hot")
+        stop_cmd = True
+    else:
+        await ctx.respond("stopped")
+
+
+@bot.command(description="stops the madness")
+async def stop(ctx):
+    stop_cmd = True
+    await ctx.respond("stopped the madness")
 
 
 bot.run(config["token"])
--- cogs/math.py
+++ cogs/math.py
@@ -6,13 +6,17 @@ class Math(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @discord.slash_command(description="Adds two numbers together")
+    @discord.slash_command(name="sum", description="Adds two numbers together")
     async def sum(self, ctx, num1: float, num2: float):
         await ctx.respond(num1 + num2)
 
-    @discord.slash_command(description="Square root of a number")
-    async def sqarert(self, ctx, num: float):
+    @discord.slash_command(name="sqrt", description="Square root of a number")
+    async def sqrt(self, ctx, num: float):
         await ctx.respond(sqrt(num))
+
+    @discord.slash_command(name="pow", description="Raises a number to a given power")
+    async def pow(self, ctx, num: float, power: float):
+        await ctx.respond(num**power)
 
 
 def setup(bot):
