68364d72a485394f28a22f0c5f14b46c3e090a91
Zedzee <65473813+transbee@users.noreply.github.com>
2022-11-13 21:00:47 -0500

    view stuff
    adsdad

--- bot.py
+++ bot.py
@@ -1,19 +1,56 @@
 import discord
 import json
+from random import choices
+from string import ascii_letters, digits
 
 
 bot = discord.Bot()
-
 
 with open("config.json") as conf:
     config = json.load(fp=conf)
 
 
 class PollMenu(discord.ui.View):
-    def __init__():
+    def __init__(self, options, title, description):
         super().__init__()
+
+        self.voted = []
+        self.options = []
+        self.embed = None
+        self.description = description
+        self.title = title
+        for opt in options:
+            self.options.append({"option": opt, "votes": 0})
+        self.option_buttons = []
+        for option in self.options:
+            self.option_buttons.append(discord.ui.Button(label=option["option"], style=discord.ButtonStyle.gray, custom_id="".join(choices(ascii_letters + digits, k=20))))
+        for button in self.option_buttons:
+            button.callback = self.__on_button_callback
+            self.add_item(button)
+
     
+    async def __on_button_callback(self, interaction: discord.Interaction):
+        for button in self.option_buttons:
+            if interaction.data["custom_id"] == button.custom_id:
+                current = self.option_buttons.index(button)
+        
+        current_option = self.options[current]
+
+        if interaction.user in self.voted:
+            await interaction.response.send_message("You already voted!", ephemeral=True)
+        else:
+            await interaction.response.send_message(f"You have voted for {current_option['option']}", ephemeral=True)
+            current_option['votes'] += 1
+            self.voted.append(interaction.user)
+            await interaction.message.edit(embed=self.create_embed())
     
+    def create_embed(self):
+        self.description = ""
+        
+        self.embed = discord.Embed(color=discord.colour.Color.random(), title=self.title, description=self.description)
+        for option in self.options:
+            self.embed.add_field(name=option["option"], value=f"{option['option']}, Voted: {option['votes']}", inline=False)
+        return self.embed
 
 
 @bot.event
--- cogs/poll.py
+++ cogs/poll.py
@@ -2,85 +2,23 @@ import discord
 from discord.ext import commands
 from random import choices
 from string import ascii_letters, digits
+from bot import PollMenu
 
 
 class Poll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.voted = []
-        self.options = []
-        self.title = ""
-        self.embed = None
     
     @discord.slash_command(description="Creates a poll. Format for options: Option1,Option2,Option3")
-    async def poll(self, ctx, title: str, options: str):
-        self.embed = None
-        self.voted = []
-        self.options = []
-        self.title = ""
-        self.title = title
-
+    async def poll(self, ctx, title: str, description: str, options: str):
         opts = options.split(sep=',')
         for opt in opts:
             opt = opt.replace(',', ' ')
 
-        for option in opts:
-            self.options.append({
-                "option": option,
-                "votes": 0
-            })
+        poll = PollMenu(title=title, options=opts, description=description)
+        embed = poll.create_embed()
 
-        description = ""
-
-        for option in self.options:
-            description += f"{option['option']}, Votes: {option['votes']}\n"
-
-        self.embed = discord.Embed(title=title, description=description, color=discord.colour.Color.random())
-        await ctx.respond("Creating poll", ephemeral=True)
-        await ctx.send(embed=self.embed, view=self.__create_view())
-    
-    def __create_view(self) -> discord.ui.View:
-        view = discord.ui.View()
-        option_buttons = []
-        for option in self.options:
-            option_buttons.append(discord.ui.Button(style=discord.ButtonStyle.gray, label=option["option"], custom_id="".join(choices(ascii_letters + digits, k=20))))
-
-
-        async def option_callback(interaction: discord.Interaction):
-            for button in option_buttons:
-                if interaction.data["custom_id"] == button.custom_id:
-                    current = option_buttons.index(button)
-
-            current_option = self.options[current]
-
-            if interaction.user in self.voted:
-                await interaction.response.send_message("You already voted!", ephemeral=True)
-            else:
-                await interaction.response.send_message(f"You have voted for {current_option['option']}", ephemeral=True)
-                current_option['votes'] += 1
-                self.voted.append(interaction.user)
-                self.embed = discord.Embed(title=self.title, description=self.__refresh_message(), color=discord.colour.Color.random())
-                await interaction.message.edit(embed=discord.Embed(title=self.title, description=self.__refresh_message(), color=discord.colour.Color.random()))
-
-        
-        for button in option_buttons:
-            button.callback = option_callback
-            
-
-        for button in option_buttons:
-            view.add_item(button)
-
-
-        return view
-    
-    def __refresh_message(self):
-        description = ""
-
-        for option in self.options:
-            description += f"{option['option']}, Votes: {option['votes']}\n"
-        
-        return description
-
+        await ctx.respond(embed=embed, view=poll)
 
 
 def setup(bot):
