12fa32c5fc70837d2b3437048e33c0a2cea9c7a8
Zedzee <65473813+transbee@users.noreply.github.com>
2022-11-17 07:40:39 -0500

    turn indicator

--- menus/game_menus/connect_four_menus.py
+++ menus/game_menus/connect_four_menus.py
@@ -260,6 +260,7 @@ class ConnectFourGame(discord.ui.View):
         embed = discord.Embed(title="Connect Four", description=str(self.board))
         if self.winner != None:
             embed.add_field(name="Winner!", value=f"{self.winner['plr']} {self.winner['color']} has won the game of Connect Four!")
+        embed.set_footer(text=f"It is {self.current_player['plr']}'s turn.")
         return embed
 
     async def button_callback_event(self, interaction: discord.Interaction):
